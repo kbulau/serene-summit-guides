db933dea3d197447ee02d0f0bf4568024b1568ca
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-29 14:15:25 -0600

    add main page components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import Camp from '@/components/Camp';
+import Features from '@/components/Features';
+import GetApp from '@/components/GetApp';
+import Hero from '@/components/Hero';
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1 className="text-teal-500 text-bold"> Hello World</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Features />
+      <GetApp />
+    </>
+  );
 }
