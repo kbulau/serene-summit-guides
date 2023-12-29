0ec693bc603769d6f2b062eceb81bd488b91db5d
kurtbulau <103153519+kbulau@users.noreply.github.com>
2023-12-29 15:32:39 -0600

    finish Features component

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -11,7 +11,7 @@ const Guide = () => {
         </p>
         <div className="flex flex-wrap justify-between gap-4 lg:gap-10">
           <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
-            Guide You to Easy Path
+            Guide You to the Easy Path
           </h2>
           <p className="regular-16 text-gray-30 xl:max-w-[520px]">
             Only with the hilink application you will no longer get lost and get
